f1dac099573d7970e6d2ab0bb126f5db0b34aed7
ZJ <zzha9494@uni.sydney.edu.au>
2022-07-05 20:18:27 +1000

    Q8

---
+++ src/Q8_String_to_Integer_atoi.java
@@ -0,0 +1,20 @@
+public class Q8_String_to_Integer_atoi {
+    public int myAtoi(String s) {
+        int sign = 1, ans = 0, index = 0;
+        char[] array = s.toCharArray();
+
+        while (index < array.length && array[index] == ' ')
+            index++;
+
+        if (index < array.length && (array[index] =='+' || array[index] == '-'))
+            sign = array[index++] == '-' ? -1 : 1;
+
+        while (index < array.length && array[index] <= '9' && array[index] >= '0') {
+            int digit = array[index++] - '0';
+            if (ans > (Integer.MAX_VALUE - digit)/10)
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            ans = ans * 10 + digit;
+        }
+        return ans * sign;
+    }
+}
