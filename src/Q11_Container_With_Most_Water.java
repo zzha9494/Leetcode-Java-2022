6bfd2d1ab79814f6da8c163ce02492e99f06ff47
ZJ <zzha9494@uni.sydney.edu.au>
2022-07-05 22:15:20 +1000

    Q11

---
+++ src/Q11_Container_With_Most_Water.java
@@ -0,0 +1,12 @@
+public class Q11_Container_With_Most_Water {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1, res = 0;
+
+        while(i != j) {
+            res = height[i] < height[j] ?
+                    Math.max(res, (j-i) * height[i++]) :
+                    Math.max(res, (j-i) * height[j--]);
+        }
+        return res;
+    }
+}
