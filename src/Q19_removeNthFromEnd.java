15aa16ced923d0b12a52ac3d688c3d3c6690034d
ZJ <zzha9494@uni.sydney.edu.au>
2022-07-13 14:00:27 +1000

    Q19

---
+++ src/Q19_removeNthFromEnd.java
@@ -0,0 +1,17 @@
+public class Q19_removeNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        for (int i = 0; i < n; i++)
+            fast = fast.next;
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
